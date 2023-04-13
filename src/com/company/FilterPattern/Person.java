422aac6fa0d04cebe9ea4dafbda75bd5899c22fa
guantianyi <vincent@scfdmkj999.onexmail.com>
2023-04-13 23:53:29 +0800

    过滤模式

---
+++ src/com/company/FilterPattern/Person.java
@@ -0,0 +1,10 @@
+package com.company.FilterPattern;
+
+public class Person {
+    private String name;
+    private String gender;
+    private String maritalStatus;
+    public Person(){
+
+    }
+}
